ca2f45e3e33c471d7f54afb9483153a04451aaff
Narankhuu Ganbaatar <narankhuuu@gmail.com>
2023-09-22 18:21:21 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+let employee = {
+    name: "Khuu",
+    streetAddress: "1510 Clarendon Blvd"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee }; 
+  
+    newEmployee[key] = value; 
+  
+    return newEmployee; 
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value; 
+  
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const clonedEmployee = { ...employee }; 
+  
+    delete clonedEmployee[key];
+  
+    return clonedEmployee; 
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]; 
+
+    return employee; 
+}
